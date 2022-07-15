f18e98897e7bbc21681407d8013a3159d237f8ad
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 16:32:48 +0200

    Implement existUser to avoid double register with same user email

--- authentication/domain/interfaces/IUserDao.ts
+++ authentication/domain/interfaces/IUserDao.ts
@@ -2,4 +2,5 @@ import {User} from "../User";
 
 export interface IUserDao {
     createUser(user: User): Promise<User>;
+    existByEmail(userEmail: string): Promise<boolean>;
 }
--- authentication/infra/dao/UserDao.ts
+++ authentication/infra/dao/UserDao.ts
@@ -2,6 +2,8 @@ import {User} from "../../domain/User";
 import {IUserDao} from "../../domain/interfaces/IUserDao";
 import {UserMapper} from "../mapper/UserMapper";
 import {UserModel} from "../schemas/UserSchema";
+import {CallbackError, HydratedDocument} from "mongoose";
+import {IUser} from "../../domain/interfaces/IUser";
 
 export class UserDao implements IUserDao {
 
@@ -10,6 +12,18 @@ export class UserDao implements IUserDao {
         await userRepository.save();
 
         return UserMapper.mapDaoDocument(userRepository);
+    }
+
+    existByEmail(userEmail: string): Promise<boolean> {
+        return new Promise(((resolve, reject) => {
+            UserModel.findOne({email: userEmail}, (_error: CallbackError, user: HydratedDocument<IUser>) => {
+                if(!user) {
+                    resolve(false);
+                    return;
+                }
+                resolve(true);
+            });
+        }));
     }
 
 }
