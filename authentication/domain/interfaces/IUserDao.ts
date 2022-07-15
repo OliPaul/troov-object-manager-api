1b6f35686234344b23ad265970d66c08fd5d1bbe
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 17:34:30 +0200

    Implement find user

--- authentication/domain/interfaces/IUserDao.ts
+++ authentication/domain/interfaces/IUserDao.ts
@@ -2,5 +2,8 @@ import {User} from "../User";
 
 export interface IUserDao {
     createUser(user: User): Promise<User>;
+
     existByEmail(userEmail: string): Promise<boolean>;
+
+    findUser(userEmail: string, userPassword: string): Promise<User | null>;
 }
--- authentication/infra/dao/UserDao.ts
+++ authentication/infra/dao/UserDao.ts
@@ -4,6 +4,7 @@ import {UserMapper} from "../mapper/UserMapper";
 import {UserModel} from "../schemas/UserSchema";
 import {CallbackError, HydratedDocument} from "mongoose";
 import {IUser} from "../../domain/interfaces/IUser";
+import {compareSync} from "bcryptjs";
 
 export class UserDao implements IUserDao {
 
@@ -17,11 +18,27 @@ export class UserDao implements IUserDao {
     existByEmail(userEmail: string): Promise<boolean> {
         return new Promise(((resolve, reject) => {
             UserModel.findOne({email: userEmail}, (_error: CallbackError, user: HydratedDocument<IUser>) => {
-                if(!user) {
+                if (!user) {
                     resolve(false);
                     return;
                 }
                 resolve(true);
+            });
+        }));
+    }
+
+    findUser(userEmail: string, userPassword: string): Promise<User | null> {
+        return new Promise(((resolve, _reject) => {
+            UserModel.findOne({email: userEmail}, (_error: CallbackError, user: HydratedDocument<IUser>) => {
+                if (!user) {
+                    resolve(null);
+                }
+
+                if (!compareSync(userPassword, user.password)) {
+                    resolve(null);
+                }
+
+                resolve(UserMapper.mapDaoDocument(user));
             });
         }));
     }
