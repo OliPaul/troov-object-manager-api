68c73045660beafadbb82f926eb1d07656d0266b
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 17:36:45 +0200

    Create sign in middleware

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ let express = require('express');
 let logger = require('morgan');
 
 let register = require('./authentication/infra/controllers/Register');
+let signIn = require('./authentication/infra/controllers/SignIn');
 
 let app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/register', register);
+app.use('/sign_in', signIn);
 
 module.exports = app;
---
+++ authentication/infra/controllers/SignIn.ts
@@ -0,0 +1,21 @@
+import {UserDao} from "../dao/UserDao";
+import {Router, Request, Response} from 'express';
+import dtoValidation from "../../../utils/dto/dtoValidation";
+import HttpException from "../../../exceptions/HttpException";
+import {SignInRequestDto} from "../dto/SignInRequestDto";
+import {SignInUser} from "../../use_cases/SignInUser";
+
+let router = Router();
+
+module.exports = router.post('/', dtoValidation(SignInRequestDto), async function (req: Request, res: Response) {
+    const signInRequestDto = new SignInRequestDto();
+    signInRequestDto.email = req.body.email;
+    signInRequestDto.password = req.body.password;
+
+    try {
+        res.send(await new SignInUser().execute(signInRequestDto, new UserDao()));
+    } catch (e) {
+        const exception = (e as HttpException);
+        res.status(exception.status).send(exception.serialize());
+    }
+});
