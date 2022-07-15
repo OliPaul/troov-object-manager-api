7e688407334afa00cc98a567b75400589d48fe13
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 13:55:27 +0200

    Crate a User domain interface with his implementation

---
+++ authentication/domain/User.ts
@@ -0,0 +1,33 @@
+import {IUser} from "./interfaces/IUser";
+
+export class User implements IUser {
+    private readonly _email: string;
+    private readonly _name: string;
+    private readonly _password: string;
+
+    constructor(email: string, name: string, password: string) {
+        this._email = email;
+        this._name = name;
+        this._password = password;
+    }
+
+    serialize(): Object {
+        return {
+            email: this._email,
+            name: this._name,
+            password: this._password
+        }
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get password(): string {
+        return this._password;
+    }
+}
