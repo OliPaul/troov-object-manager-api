3d8d8047bd3debf0ab81ce9a1e18233f65f690b9
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 22:06:36 +0200

    Implement update item use case

---
+++ item/use_cases/UpdateItem.ts
@@ -0,0 +1,12 @@
+import {IItemDao} from "../domain/interfaces/IItemDao";
+import {ItemResponseDto} from "../infra/dto/ItemResponseDto";
+import {Item} from "../domain/Item";
+import {UpdateItemRequestDto} from "../infra/dto/UpdateItemRequestDto";
+
+export class UpdateItem {
+    async execute(itemId: string, updateItemRequestDto: UpdateItemRequestDto, itemDao: IItemDao): Promise<ItemResponseDto> {
+        let item: Item = await itemDao.update(itemId, updateItemRequestDto.serialize());
+
+        return new ItemResponseDto(item.name, item.description, item.id, item.createdAt, item.updatedAt);
+    }
+}
