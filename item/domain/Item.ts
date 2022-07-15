99fc352711cddadd6ea9e02c89d63b75c58f1fd8
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 21:08:07 +0200

    Refacto properties

--- item/domain/Item.ts
+++ item/domain/Item.ts
@@ -3,22 +3,34 @@ import {IItem} from "./interfaces/IItem";
 export class Item implements IItem {
     private _createdAt: Date | undefined;
     private readonly _description: string;
-    private readonly _id: string;
+    private _id: string | undefined;
     private readonly _name: string;
     private _updatedAt: Date | undefined;
 
 
-    constructor(description: string, id: string, name: string) {
+    constructor(description: string, name: string, id?: string, createdAt?: Date, updatedAt?: Date) {
         this._description = description;
-        this._id = id;
         this._name = name;
+        this._id = id;
+        this._createdAt = createdAt;
+        this._updatedAt = updatedAt;
+    }
+
+    serialize(): Object {
+        return {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt
+        }
     }
 
     get description(): string {
         return this._description;
     }
 
-    get id(): string {
+    get id(): string | undefined {
         return this._id;
     }
 
@@ -35,10 +47,13 @@ export class Item implements IItem {
         return this._updatedAt;
     }
 
+    set id(value: string | undefined) {
+        this._id = value;
+    }
+
     set createdAt(value: Date | undefined) {
         this._createdAt = value;
     }
-
 
     set updatedAt(value: Date | undefined) {
         this._updatedAt = value;
--- item/domain/interfaces/IItem.ts
+++ item/domain/interfaces/IItem.ts
@@ -1,5 +1,5 @@
 export interface IItem {
-    id: string;
+    id?: string;
     name: string;
     description: string;
     createdAt?: Date;
