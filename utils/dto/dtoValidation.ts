16f667de0b49b8948a3df86311244c9bfd95e8df
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 14:03:43 +0200

    Implement DTO validation middleware for incoming data validation before going to our domain

---
+++ utils/dto/dtoValidation.ts
@@ -0,0 +1,29 @@
+import {RequestHandler, Request, Response} from "express";
+import {plainToInstance} from "class-transformer";
+import {validate, ValidationError} from "class-validator";
+import {sanitize} from "class-sanitizer";
+import HttpException from "../../exceptions/HttpException";
+
+function dtoValidation(type: any, skipMissingProperties = false): RequestHandler {
+
+    return (req: Request, res: Response, next) => {
+        const dtoObject = plainToInstance(type, req.body);
+        validate(dtoObject, {skipMissingProperties}).then(
+            (errors: ValidationError[]) => {
+                if (errors.length > 0) {
+                    // Get the first error encountered
+                    const dtoErrors = (Object as any).values(errors[0].constraints)[0];
+                    res.send(new HttpException(400, dtoErrors).serialize());
+                } else {
+                    // Sanitize the object and call the next middleware
+                    sanitize(dtoObject);
+                    req.body = dtoObject;
+                    next();
+                }
+            }
+        );
+    };
+}
+
+
+export default dtoValidation;
