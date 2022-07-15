c3687bc4d93c8aaa9c94c5c94be634c260c59a61
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-07-15 17:36:26 +0200

    Implement sign in user use case

---
+++ authentication/use_cases/SignInUser.ts
@@ -0,0 +1,18 @@
+import {IUserDao} from "../domain/interfaces/IUserDao";
+import {SignInRequestDto} from "../infra/dto/SignInRequestDto";
+import {UserNotFoundException} from "../domain/exceptions/UserNotFoundException";
+import {JwtUtils} from "../../utils/jwt/JwtUtils";
+import {SignInResponseDto} from "../infra/dto/SignInResponseDto";
+
+export class SignInUser {
+    async execute(signInRequestDto: SignInRequestDto, userDao: IUserDao): Promise<SignInResponseDto> {
+        const user = await userDao.findUser(signInRequestDto.email!, signInRequestDto.password!);
+
+        if(!user) {
+            throw new UserNotFoundException(404, "Identifiants invalides.");
+        }
+
+        const token = JwtUtils.sign(user.serialize())
+        return new SignInResponseDto(user.name, user.email, token);
+    }
+}
